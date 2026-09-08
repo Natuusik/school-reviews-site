97be8ef6f3c2a569a261f46b8bf464ee59e6f938
Natalja <NataljaSemitseva@outlook.com>
2026-09-08 18:34:04 +0300

    fix database name string

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '123456', 
-    database: process.env.DB_NAME || 'minisite_db',
+    database: process.env.DB_NAME || 'defaultdb',
     port: process.env.DB_PORT || 3306
 });
 
